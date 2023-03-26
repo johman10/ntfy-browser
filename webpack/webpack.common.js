e6a8965e22103320e0df9188d1894e9656bf6bf7
Johan van Eck <johveck@gmail.com>
2023-03-26 13:58:57 +0300

    Cross browser support - can build for Chrome and Firefox now

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,19 +2,34 @@ const webpack = require("webpack");
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const srcDir = path.join(__dirname, "..", "src");
+const WextManifestWebpackPlugin = require("wext-manifest-webpack-plugin");
+
+const TARGET_BROWSER = process.env.TARGET_BROWSER;
 
 module.exports = {
   entry: {
-    popup: path.join(srcDir, "popup.tsx"),
-    options: path.join(srcDir, "options.tsx"),
-    background: path.join(srcDir, "background.ts"),
+    manifest: path.join(srcDir, "manifest.json"),
+    "js/popup": path.join(srcDir, "popup.tsx"),
+    "js/options": path.join(srcDir, "options.tsx"),
+    "js/background": path.join(srcDir, "background.ts"),
   },
   output: {
-    path: path.join(__dirname, "../dist/js"),
+    path: path.join(__dirname, `../dist/${TARGET_BROWSER}`),
     filename: "[name].js",
   },
   module: {
     rules: [
+      {
+        type: "javascript/auto",
+        test: /manifest\.json$/,
+        use: {
+          loader: "wext-manifest-loader",
+          options: {
+            usePackageJSONVersion: true,
+          },
+        },
+        exclude: /node_modules/,
+      },
       {
         test: /\.tsx?$/,
         use: "ts-loader",
@@ -23,12 +38,13 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: [".ts", ".tsx", ".js", ".json"],
   },
   plugins: [
     new CopyPlugin({
-      patterns: [{ from: ".", to: "../", context: "public" }],
+      patterns: [{ from: ".", to: "./", context: "public" }],
       options: {},
     }),
+    new WextManifestWebpackPlugin(),
   ],
 };
